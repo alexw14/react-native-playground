5be555677c99325fb6fad66d7eb09296edeb699c
Alex Wong <alexw14@gmail.com>
2025-01-23 18:10:53 +0800

    add routes models middlewares

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,8 +1,18 @@
+require('./models/User');
+require('./models/Track');
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv').config();
+const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRoutes);
 
 mongoose.connect(process.env.mongoUri);
 
@@ -13,7 +23,7 @@ mongoose.connection.on('error', (error) => {
   console.error('Error connecting to mongo', error);
 });
 
-app.get('/', (req, res) => {
+app.get('/', requireAuth, (req, res) => {
   res.send('hello');
 });
 
